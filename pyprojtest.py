fcfbfa98c7f155cfcf74219efb3aa8b82d560bb9
pablonieves1 <110918702+pablonieves1@users.noreply.github.com>
2025-06-21 15:39:07 +0200

    Edited to eliminate conflicts with GH desktop

--- pyprojtest.py
+++ pyprojtest.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pyproj
 import geopandas
 import os
@@ -6,3 +7,13 @@ import os
 pyproj_datadir = pyproj.datadir.get_data_dir()
 print(pyproj_datadir)
 
+=======
+import pyproj
+import geopandas
+import os
+
+
+pyproj_datadir = pyproj.datadir.get_data_dir()
+print(pyproj_datadir)
+
+>>>>>>> 3683fbb36275adddd3eb8cb0935a342a7d7dd4d2
